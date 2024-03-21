d8f25df18407310920e1cd80e65d9049efae7e81
JoeyOrm <joitoomar@gmail.com>
2024-03-21 09:57:41 -0400

    Code Cleanup

--- Entity.py
+++ Entity.py
@@ -1,18 +1,14 @@
 import pygame
 class Entity:
-    def __init__(self, name, health=100, dmg=15, lvl=1 ):
-        self.action_sprites = {}
-        self.current_action = "idle" 
-        self.current_frame = 0  
-        self.rect = self.rect = pygame.Rect(100, 50, 32, 32)
-        self.speed = 10
+    def __init__(self, name, health=100, dmg=15, lvl=1, x=10, y=10 ):
         
         self._name = name
         self.health = health
         self.dmg = dmg
         self.lvl = lvl
         self.dead = False
-    
+        
+        
     @property
     def name(self):
         return self._name
@@ -50,46 +46,7 @@ class Entity:
     @dead.setter
     def dead(self, value):
         self.dead = value
-    
-    @property
-    def speed(self):
-        return self.speed
-    @speed.setter
-    def speed(self, value):
-        self.speed = value
-        
-    def load_and_split_sprite_sheet(self, filename, num_frames):
-        spritesheet = pygame.image.load(filename).convert_alpha()  
-        frames = []
-        width = spritesheet.get_width() // num_frames 
-        for i in range(num_frames):
-            frame_rect = (i * width, 0, width, spritesheet.get_height())
-            frames.append(spritesheet.subsurface(frame_rect))
-        return frames
-    
-    def load_sprites(self, action_name, sprite_sheet):
-        self.action_sprites[action_name] = self.load_and_split_sprite_sheet(sprite_sheet, 1)
-        
-    def update(self):  
-        self.handle_animation()
-        self.handle_movement() 
 
-    def handle_animation(self):
-        self.current_frame += 1
-        if self.current_frame >= len(getattr(self, f"images_{self.current_action}")):
-            self.current_frame = 0
-        self.image = getattr(self, f"images_{self.current_action}")[self.current_frame]
-    
-    def handle_movement(self):
-        keys = pygame.key.get_pressed() 
-        if keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
-        if keys[pygame.K_UP]:
-            self.rect.y -= self.speed
-        if keys[pygame.K_DOWN]:
-            self.rect.y += self.speed  
     
     def take_damage(self):
         self.health -= self.dmg
